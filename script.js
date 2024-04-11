8d1db8adf42faf8f9b5b21a25f30765394156b88
50reina <nrr.nya@gmail.com>
2024-04-10 20:35:51 -0700

    changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //alert("Hello World");
 
-console.log("Person name is " + Bean);
+//Activity 2
 
 let personName = "Bean";
 let personAge = "12";
@@ -8,3 +8,8 @@ let personiskid = true;
 let knowsJavaScript = false;
 let hobbies; // Bean's hobbies are not defined yet
 let previousEmployer = null; // Bean is student
+
+console.log("Person name is: " + Bean);
+
+// Activity 3
+let tido_list = ["tax return", "go to embassy", "change nail"];
