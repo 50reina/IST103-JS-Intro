0737272fa7153f04da5d8c1ce540389bbeba7e86
50reina <nrr.nya@gmail.com>
2024-04-10 19:44:18 -0700

    added variables at script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Hello World");
+//alert("Hello World");
+
+let personName = "Bean";
+let personAge = "12";
+let personiskid = true;
+let knowsJavaScript = false;
+let hobbies; // Bean's hobbies are not defined yet
+let previousEmployer = null; // Bean is student
